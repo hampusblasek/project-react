c11377cc1d5d3a9c3b102c0b7ed8cfd15b7d088a
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-11-15 10:53:57 +0100

    Har uppdaterat header med style

--- matt-och-go/src/Components/Header.jsx
+++ matt-och-go/src/Components/Header.jsx
@@ -5,15 +5,15 @@ export function Header() {
   return (
     <>
       <header>
+        
         <h1 className="logo">Mätt och Go!</h1>
-        <div className="nav-box">
             <nav>
                 <Link className="link" to="/">Hem</Link>
                 <Link className="link" to="category">Världens kök</Link>
                 <Link className="link" to="saved-recipes">Sparade recept</Link>
                 <Link className="link" to="about-us">Om Oss</Link>
             </nav>
-        </div>
+        
       </header>
     </>
   );
--- matt-och-go/src/Pages/Recipe.jsx
+++ matt-och-go/src/Pages/Recipe.jsx
@@ -9,7 +9,21 @@ export function RecipePage({ recipes, recData }) {
   return (
     <>
       <div className="recipe-container">
-        <p>{food.name}</p>
+        <div className="recipe-box">
+          <div className="rec-info">
+            <h1 className="rec-title">{food.name}</h1>
+            <div className="info">
+              <img className="food-icon" src="/time-green.svg" alt="" />
+              <span>{food.cookTimeMinutes + food.prepTimeMinutes} minuter</span>
+              <span>{food.mealType +""}</span>
+              <img src="" alt="" />
+              <span></span>
+            </div>
+          </div>
+          <div className="rec-img">
+            <img className="food-img" src={food.image} alt="" />
+          </div>
+        </div>
       </div>
     </>
   );
