cc38156d73b7150288bcf2e84f2faaa243b02447
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-11-21 12:14:19 +0100

    formatering

--- matt-och-go/src/Components/Rating.jsx
+++ matt-och-go/src/Components/Rating.jsx
@@ -4,20 +4,17 @@ import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
 
-export function BasicRating({food}) {
+export function BasicRating({ food }) {
   const [value, setValue] = useState([]);
   console.log(food);
-  
 
   return (
-    <Box sx={{ "& > legend": { mt: 2 } }}>
-     
-     <Rating
+    <Box>
+      <Rating
         name="simple-controlled"
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
-          
         }}
         size="large"
       />
