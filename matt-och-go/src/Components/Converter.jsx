7feacd5615814fad1639784cf4be3a0c21113b4e
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-11-22 14:20:07 +0100

    Mått omvandlaren är färdig, men behöver mer style

--- matt-och-go/src/Components/Converter.jsx
+++ matt-och-go/src/Components/Converter.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "../CSS/converter.css";
 
-
 export function MessurementConverter() {
   const [value1, setValue1] = useState(null);
   const [value2, setValue2] = useState(null);
@@ -11,43 +10,20 @@ export function MessurementConverter() {
   const dl = 100;
   let sum = value2 / value1;
 
-  if (!sum) {
-    return (
-      <>
-        <h2>Använd gärna våran måttomvandlare</h2>
-        <form action="">
-          <select
-            onChange={(event) => setValue1(event.target.value)}
-            name="messure"
-            id="messure"
-          >
-            <option>Välj</option>
-            <option value={dl}>Deciliter</option>
-            <option value={msk}>Matskedar</option>
-            <option value={tsk}>Teskedar</option>
-            <option value={krm}>Kryddmått</option>
-          </select>
-          <select
-            onChange={(event) => setValue2(event.target.value)}
-            name="messure"
-            id="messure"
-          >
-            <option>Välj</option>
-            <option value={dl}>Deciliter</option>
-            <option value={msk}>Matskedar</option>
-            <option value={tsk}>Teskedar</option>
-            <option value={krm}>Kryddmått</option>
-          </select>
-        </form>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div>
-          <p>
-            Det går <span>{sum}</span>
-          </p>
+  const numberOfSum = () => {
+    if (!sum) {
+      return "Välj mått ovan för att se ditt svar här!";
+    } else {
+      return "Svar: " + sum;
+    }
+  };
+
+  return (
+    <>
+      <div className="converter-container">
+        <h2 className="converter-title">Använd gärna våran måttomvandlare</h2>
+        <div className="converter-box">
+          <p>Hur många</p>
           <form action="">
             <select
               onChange={(event) => setValue1(event.target.value)}
@@ -60,7 +36,7 @@ export function MessurementConverter() {
               <option value={tsk}>Teskedar</option>
               <option value={krm}>Kryddmått</option>
             </select>
-            <p>på en</p>
+            <p> går det på en</p>
             <select
               onChange={(event) => setValue2(event.target.value)}
               name="messure"
@@ -74,7 +50,10 @@ export function MessurementConverter() {
             </select>
           </form>
         </div>
-      </>
-    );
-  }
+        <p className="converter-answer">
+          <span>{numberOfSum()}</span>
+        </p>
+      </div>
+    </>
+  );
 }
