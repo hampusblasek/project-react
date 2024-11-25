37c15f53785bef48ce59acb57daf8819ee17fe10
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-11-25 14:45:41 +0100

    Lagt in så att logan är klickbar

--- matt-och-go/src/Components/Header.jsx
+++ matt-och-go/src/Components/Header.jsx
@@ -1,39 +1,49 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { saveRecState } from "../App";
+import { Navigate } from "react-router-dom";
 import "../CSS/header.css";
 
 export function Header() {
   const [saveRec, setSaveRec] = useRecoilState(saveRecState);
+  const navigateTo = useNavigate();
+
   const savedResNumber = () => {
-    if(saveRec.length > 0){
-      return saveRec.length
-    } else{
-      return null
+    if (saveRec.length > 0) {
+      return saveRec.length;
+    } else {
+      return null;
     }
-  }
+  };
   const savedResHeart = () => {
-    if(saveRec.length > 0){
-      return "/heart.svg"
-    } else{
-      return null
+    if (saveRec.length > 0) {
+      return "/heart.svg";
+    } else {
+      return null;
     }
-  }
+  };
   return (
     <>
       <header>
-        
-        <div className="logo-box">
+        <div onClick={() => navigateTo("/")} className="logo-box">
           <h1 className="logo">Mätt och Go!</h1>
           <img className="logo-img" src="/logo2.png" alt="" />
         </div>
-            <nav>
-                <NavLink className="link" to="/">Hem</NavLink>
-                <NavLink className="link" to="category">Världens kök</NavLink>
-                <NavLink className="link" to="saved-recipes">Sparade recept <img src={savedResHeart()} alt="" /> <span>{savedResNumber()}</span></NavLink>
-                <NavLink className="link" to="about-us">Om Oss</NavLink>
-            </nav>
-        
+        <nav>
+          <NavLink className="link" to="/">
+            Hem
+          </NavLink>
+          <NavLink className="link" to="category">
+            Världens kök
+          </NavLink>
+          <NavLink className="link" to="saved-recipes">
+            Sparade recept <img src={savedResHeart()} alt="" />{" "}
+            <span>{savedResNumber()}</span>
+          </NavLink>
+          <NavLink className="link" to="about-us">
+            Om Oss
+          </NavLink>
+        </nav>
       </header>
     </>
   );
