5e5e6ecdee6f08291052f26a74fe6acb6dc9d0e3
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-11-25 11:13:33 +0100

    fixat error-komponent ifall en sida inte hittas

--- matt-och-go/src/Components/errorPage.jsx
+++ matt-och-go/src/Components/errorPage.jsx
@@ -1,3 +1,26 @@
-export function Error(){
-    return <p>Något gick fel</p>
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../CSS/error.css";
+
+export function Error() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="error-container">
+        <div className="error-box">
+          <div className="error-text">
+            <h2 className="error-title">Oops! Sidan du söker hittades inte</h2>
+            <p className="error-p">
+              Tryck på knappen nedan för att komma tillbaka till startsidan
+            </p>
+            <button onClick={() => navigate("/")} className="error-btn">
+              Tillbaka till startsidan
+            </button>
+          </div>
+          <img className="error-img" src="/chef-big.svg" alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
