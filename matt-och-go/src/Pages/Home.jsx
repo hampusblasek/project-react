4bca89e661bbde37c657eef6e70baca5c071bad0
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-12-03 15:57:05 +0100

    början på dropdown

--- matt-och-go/src/Pages/Home.jsx
+++ matt-och-go/src/Pages/Home.jsx
@@ -8,6 +8,9 @@ import { recipeState } from "../App";
 export function HomePage({ levelImg }) {
   const [recipes] = useRecoilState(recipeState); // state som innehåller alla recept
 
+  const shortList = recipes.slice(0, 4);
+  console.log(shortList);
+
   if (!recipes) {
     return <p>Laddar...</p>; // om det inte finns några recept
   }
@@ -16,7 +19,43 @@ export function HomePage({ levelImg }) {
     <>
       <div className="home-container">
         <div className="hero">
-          <h2 className="hero-text">Vad är du sugen på?</h2>
+          {/* <h2 className="hero-text">Vad är du sugen på?</h2> */}
+          <input className="input-search" type="text" />
+          <div className="drop-down-search">
+            <div className="drop-box">
+              {shortList.map((short) => (
+                <div className="card-drop">
+                  <img className="img-search" src={short.image} alt="" />
+                  <div>
+                    <p>{short.name}</p>
+                    <div className="card-bottom-search">
+                      <div className="bottom-section">
+                        <img
+                          className="card-icon"
+                          src="/time-green.svg"
+                          alt="En klocka"
+                        />
+                        <p className="card-time-search">
+                          {short.cookTimeMinutes + short.prepTimeMinutes}{" "}
+                          minuter
+                        </p>
+                      </div>
+                      <div className="bottom-section">
+                        <img
+                          className="card-icon"
+                          src={levelImg(short.difficulty)}
+                          alt=""
+                        />
+                        {/*  <img className="card-icon" src="/hard.svg" alt="" /> */}
+                        <p>{short.difficulty}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <div className="show-more">Visa fler</div>
+            </div>
+          </div>
         </div>
         <h2 className="h2-title">Färdigt på under 40 min!</h2>
         <Under40 levelImg={levelImg} recipes={recipes} />
