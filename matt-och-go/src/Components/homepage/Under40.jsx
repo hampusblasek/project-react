ec46d89a164920f300aeb694ec08ac19c082fca8
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-12-04 17:22:21 +0100

    knapparna göms när man inte kan bläddra åt respektive håll

--- matt-och-go/src/Components/homepage/Under40.jsx
+++ matt-och-go/src/Components/homepage/Under40.jsx
@@ -1,8 +1,9 @@
 import { Cards } from "../cards";
 import "../../CSS/home.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export function Under40({ recipes }) {
+  const [scrollState, setScrollState] = useState(0)
   const scroll = useRef();
 
   const fastList = recipes.filter(
@@ -16,30 +17,36 @@ export function Under40({ recipes }) {
   const scrollDirection = (direction) => {
     if (direction === "left") {
       scroll ? (scroll.current.scrollLeft -= 290) : null;
+      setScrollState(scrollState - 1)
     } else {
       scroll ? (scroll.current.scrollLeft += 290) : null;
+      setScrollState(scrollState + 1)
     }
   };
   return (
     <>
       <div className="under40-container">
-        <button
-          onClick={() => scrollDirection("left")}
-          className="scroll-btn-next"
-        >
-          <img width="35px" src="/arrow-left.svg" alt="pil som pekar åt vänster" />
-        </button>
+        <div className="btn-div">
+          <button
+            onClick={() => scrollDirection("left")}
+            className={scrollState > 0 ? "scroll-btn-next" : "hide"}
+          >
+            <img width="35px" src="/arrow-left.svg" alt="pil som pekar åt vänster" />
+          </button>
+        </div>
         <div className="home-box" ref={scroll}>
           {fastList.map((food, index) => (
             <Cards key={index} food={food} />
           ))}
         </div>
-        <button
-          onClick={() => scrollDirection("right")}
-          className="scroll-btn-next"
-        >
-          <img width="35px" src="/arrow-right.svg" alt="pil som pekar åt höger" />
-        </button>
+        <div className="btn-div">
+          <button
+            onClick={() => scrollDirection("right")}
+            className={scrollState > 8 ? "hide" : "scroll-btn-next"}
+          >
+            <img width="35px" src="/arrow-right.svg" alt="pil som pekar åt höger" />
+          </button>
+        </div>
       </div>
     </>
   );
