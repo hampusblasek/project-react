93f1cffc79ea332f8e664675ff8417c253b7aaf0
Hampus.Blasek <Hampus_blasek@hotmail.com>
2024-12-06 13:24:35 +0100

    Utökat funktionalitet för kategorier

--- matt-och-go/src/Pages/Category.jsx
+++ matt-och-go/src/Pages/Category.jsx
@@ -2,40 +2,118 @@ import "../CSS/category.css";
 import { Cards } from "../Components/cards";
 import { useRecoilState } from "recoil";
 import { recipeState } from "../App";
+import { useState } from "react";
 
-export function Categories({ setCatData, catData, setTitle, title, levelImg }) {
-  const [recipes, setRecipes] = useRecoilState(recipeState);
-  const catList = [];
+export function Categories({
+  setCatData,
+  catData,
+  setTitle,
+  title,
+  setTitle2,
+  title2,
+}) {
+  const [recipes] = useRecoilState(recipeState);
+  const [cuisineState, setCuisineState] = useState(false);
+  const [typeState, setTypeState] = useState(false);
+  const [allState ,setAllState] = useState(false);
+
+  const cuisineList = [];
   for (let recipe of recipes) {
-    if (!catList.includes(recipe.cuisine)) { // Här filtreras alla dubbletter ut
-      catList.push(recipe.cuisine);
+    if (!cuisineList.includes(recipe.cuisine)) {
+      // Här filtreras alla dubbletter ut
+      cuisineList.push(recipe.cuisine);
     }
   }
-  const filterCat = (category) => {
+  const filterCuisine = (category) => {
+    setCatData("");
     setCatData(recipes.filter((value) => value.cuisine === category)); // Här filtreras alla recept ut som inte stämmer överräns med vald kategori
     setTitle(category); // Sätter titeln efter vald kategori
+
     return catData;
+  };
+  const filterType = (type) => {
+    setCatData("");
+    setCatData(recipes.filter((value) => value.mealType.includes(type))); // Här filtreras alla recept ut som inte stämmer överräns med vald kategori
+    if (type == "Dinner") {
+      setTitle("Middag"); // Sätter titeln efter vald kategori
+    } else if (type == "Beverage") {
+      setTitle("Dryck");
+    } else {
+      setTitle(type);
+    }
+
+    return catData;
+  };
+  const mainFilter = (value) => {
+    if (value === "cuisine") {
+      setCuisineState(true);
+      setTypeState(false);
+      setAllState(false);
+      setTitle2("Vald kategori: Världens kök");
+    } else if (value === "type") {
+      setCuisineState(false);
+      setTypeState(true);
+      setAllState(false);
+      setTitle2("Vald kategori: Måltid");
+    } else {
+      setCatData(recipes);
+      setCuisineState(false);
+      setTypeState(false);
+      setAllState(true);
+      setTitle("Alla recept");
+      setTitle2("Vald kategori: Alla recept");
+    }
   };
 
   return (
     <>
       <div className="category-container">
         <h1 className="category-title">Välj en kategori</h1>
+        <h2 className="text-title2">{title2}</h2>
+        <div className="category-btn-box">
+          <button
+            className="category-btn"
+            onClick={() => mainFilter("cuisine")}
+          >
+            Världens kök
+          </button>
+          <button className="category-btn" onClick={() => mainFilter("type")}>
+            Måltid
+          </button>
+          <button className="category-btn" onClick={() => mainFilter("all")}>
+            Alla recept
+          </button>
+        </div>
         <div className="category-container1">
-          {catList.map((category) => (
+          {cuisineList.map((category, index) => (
             <div
-              key={category}
-              onClick={() => filterCat(category)}
-              className="category-box"
+              key={index}
+              onClick={() => filterCuisine(category)}
+              className={cuisineState ? "category-box" : "display-none"}
             >
               <p>{category}</p>
             </div>
           ))}
+
+          <div className={typeState ? "category-box-1" : "display-none"}>
+            <p onClick={() => filterType("Lunch")} className="type-meal">
+              Lunch
+            </p>
+            <p onClick={() => filterType("Dinner")} className="type-meal">
+              Middag
+            </p>
+            <p onClick={() => filterType("Dessert")} className="type-meal">
+              Dessert
+            </p>
+            <p onClick={() => filterType("Beverage")} className="type-meal">
+              Dryck
+            </p>
+          </div>
         </div>
         <h1 className="category-title">{title}</h1>
         <div className="cuisine-container">
           {catData.map((food, index) => (
-            <Cards key={index} food={food} levelImg={levelImg} />
+            <Cards key={index} food={food} />
           ))}
         </div>
       </div>
